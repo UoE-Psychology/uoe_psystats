82d4c92e9615c569697754589eb9b8f5a2bb72fc
Josiah King <josiahpjking@gmail.com>
2020-04-08 13:32:45 +0100

    index edits icons

--- makeindex.py
+++ makeindex.py
@@ -87,9 +87,9 @@ INDEX_TEMPLATE = r"""
 """
 
 
-EXCLUDED = ['index.html','style.css','readme.txt','images','fonts','default.css']
+EXCLUDED = ['index.html','style.css','readme.txt','images','fonts','default.css','data','functions','labsheets']
 EXCLUDEDDIR = ['book','book_sols']
-    
+
 dlabs={'': 'Home',
        'dapr1':'Data Analysis in Psychology using R 1',
        'dapr2':'Data Analysis in Psychology using R 2',
@@ -102,20 +102,20 @@ dlabs={'': 'Home',
 #<h2><a href="${name}">${dirlabels[name]}</a></h2>
 
 ddesc={'': 'The Department of Psychology at the University of Edinburgh offers 3 Undergraduate Courses in statistics, and 2 postgraduate courses. ',
-       'dapr1':'Data Analysis in Psychology using R 1 provides foundations in working with data, probability, hypothesis testing and the use of R statistical programming environment.<br><br>The course is taught based on a mixture of lectures, labs and structured independent learning tasks.<br>In semester 1, lectures cover fundamental principles of describing data and of probability theory. In semester 2, lectures build up to discussion of how we make inferences about our hypotheses in psychology, dealing with probability distributions, sampling and hypothesis testing. The course then introduces simple statistical tests for two variables by way of example.<br><br>In labs, the course introduces the fundamental principles of R programming, with a focus on understanding in a general way how R works, such that these principles can be applied to the use of R for applied data analysis. Students apply this learning to topics such as basic calculation, data management, plotting and use of simple statistical tests.',
+       'dapr1':'Data Analysis in Psychology using R-1 is a year-long course providing foundations in working with data, probability, hypothesis testing and the use of R.<br><br>The course is taught based on a mixture of lectures, labs and structured independent learning tasks.<br>The course begins by covering the fundamental principles of describing data and of probability theory. It then builds to a discussion of how we make inferences about our hypotheses in psychology, dealing with probability distributions, sampling and hypothesis testing, and introduces simple statistical tests for two variables by way of example.<br><br>The labs also introduces the fundamental principles of programming in R, taking students from basic calculations to the foundations of data management, plotting and use of simple statistical tests.',
        
-       'dapr2':'Data Analysis in Psychology using R 2 is the ....',
-       'dapr3':'Data Analysis in Psychology using R 3 is the ....',
+       'dapr2':'Data Analysis in Psychology using R 2 will arrive in the 2020-21 academic year.',
+       'dapr3':'Data Analysis in Psychology using R 3 will arrive in the 2021-22 academic year.',
        
-       'usmr':'Univariate Statistics & Methodology in R is a semester long course that is taught using a combination of lab and lecture sessions and is suitable for students following Masters programmes in Psychology and Linguistics. It starts with an introduction to basic statistics and the basics of the R programme, and will give student competence in the standard univariate methodology and analysis using R.<br><br>Design and analysis are taught under a unifying framework which shows a) how research problems and design should inform which statistical method to use and b) that all statistical methods are special cases of a more general model. The course concentrates on research designs and analysis for problems in which there is a single outcome variable and would be taught using the general linear model as a framework to design and analysis.<br><br>Content covered includes: Introduction to the use of statistical methods in research; Introduction to R for statistics; Refresher in inferential statistics including Hypothesis testing, Type I vs. Type II errors, p-values, power, correlation, chi-squares; Linear regression: including diagnostics, transformation, different families of models; Multiple regression: extending linear regression to multiple IVs and including interactions, effects coding; The general linear model (GLM) as an inclusive framework (including ANOVA, ANCOVA, mixed designs)',
+       'usmr':'Univariate Statistics & Methodology in R is a semester long course that is provided for students following Masters programmes in Psychology and Linguistics. It starts with an introduction to basic statistics and the basics of R programming, and will give student competence in the standard univariate methodology and analysis using R.<br><br>Design and analysis are taught under a unifying framework which shows a) how research problems and design should inform which statistical method to use and b) that all statistical methods are special cases of a more general model. The course concentrates on research designs and analysis for problems in which there is a single outcome variable and would be taught using the general linear model as a framework to design and analysis.<br><br>Content covered includes: Introduction to the use of statistical methods in research; Introduction to R for statistics; Refresher in inferential statistics including Hypothesis testing, Type I vs. Type II errors, p-values, power, correlation, chi-squares; Linear regression: including diagnostics, transformation, different families of models; Multiple regression: extending linear regression to multiple IVs and including interactions, effects coding; The general linear model (GLM) as an inclusive framework (including ANOVA, ANCOVA, mixed designs)',
        
        'multivar':'Multivariate Statistics & Methodology in R is a semester long course providing an advanced level overview of statistical analysis techniques and methodology issues relevant to psychological research. The course builds on the concepts and skills developed in Univariate Statistics and Methodology using R.<br><br>Multivariate Statistics and Methodology using R uses a problem-oriented approach to introduce analysis tools that extend to cases where multiple outcome variables are being studied simultaneously, and cases where the data contain a structure, e.g. children nested in classes, in schools, or more complex cases.<br><br>Content covered includes: Linear Mixed Modelling; Growth Curve Analysis; Principal Component Analysis; Factor Analysis; Path Analysis; Structural Equation Modelling',
        
-       'dapr1lectures':'Lecture Slides is the ....',
-       'dapr2lectures':'Lecture Slides is the ....',
-       'dapr3lectures':'Lecture Slides is the ....',
-       'usmrlectures':'Lecture Slides is the ....',
-       'multivarlectures':'Lecture Slides is the ....'
+       'dapr1lectures':'Coming soon..',
+       'dapr2lectures':'Coming soon..',
+       'dapr3lectures':'Coming soon..',
+       'usmrlectures':'Coming soon..',
+       'multivarlectures':'Coming soon..'
        }
 
 
