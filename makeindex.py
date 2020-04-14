b5031e363f59ec340e0b22b2c266973ffb2a1d06
Josiah King <josiahpjking@gmail.com>
2020-04-14 16:29:03 +0100

    update indexes again. considering margin notes for book

--- makeindex.py
+++ makeindex.py
@@ -57,7 +57,7 @@ INDEX_TEMPLATE = r"""
                 <li><a href="../">Back</a></li>
             </ul>
             <div class="containerdirs">
-            <div class="description">
+            <div class="description-${headcol}">
                 <h3>${dirdesc.get(headcol2,"")}</h3>
             </div>
 			% for name in dirnames:
@@ -88,6 +88,7 @@ INDEX_TEMPLATE = r"""
 
 
 EXCLUDED = ['index.html','style.css','readme.txt','images','fonts','default.css','data','functions','labsheets']
+INCLUDED = ['dapr1','dapr2','dapr3','usmr','multivar','book','lectures']
 EXCLUDEDDIR = ['book','book_sols']
 
 dlabs={'': 'Home',
@@ -102,12 +103,13 @@ dlabs={'': 'Home',
 #<h2><a href="${name}">${dirlabels[name]}</a></h2>
 
 ddesc={'': 'The Department of Psychology at the University of Edinburgh offers 3 Undergraduate Courses in statistics, and 2 postgraduate courses. ',
-       'dapr1':'Data Analysis in Psychology using R-1 is a year-long course providing foundations in working with data, probability, hypothesis testing and the use of R.<br><br>The course is taught based on a mixture of lectures, labs and structured independent learning tasks.<br>The course begins by covering the fundamental principles of describing data and of probability theory. It then builds to a discussion of how we make inferences about our hypotheses in psychology, dealing with probability distributions, sampling and hypothesis testing, and introduces simple statistical tests for two variables by way of example.<br><br>The labs also introduces the fundamental principles of programming in R, taking students from basic calculations to the foundations of data management, plotting and use of simple statistical tests.',
        
-       'dapr2':'Data Analysis in Psychology using R 2 will arrive in the 2020-21 academic year.',
-       'dapr3':'Data Analysis in Psychology using R 3 will arrive in the 2021-22 academic year.',
+       'dapr1':'Data Analysis in Psychology using R-1 is a year-long course providing foundations in working with data and probability theory, and building to learning about how we make inferences about our hypotheses in psychology. We will deal with probability distributions, sampling and hypothesis testing, introducing simple statistical tests for two variables by way of example.<br><br>These concepts are demonstrated in R throughout, taking students from basic calculations to the foundations of data management, plotting and use of simple statistical tests.',
        
-       'usmr':'Univariate Statistics & Methodology in R is a semester long course that is provided for students following Masters programmes in Psychology and Linguistics. It starts with an introduction to basic statistics and the basics of R programming, and will give student competence in the standard univariate methodology and analysis using R.<br><br>Design and analysis are taught under a unifying framework which shows a) how research problems and design should inform which statistical method to use and b) that all statistical methods are special cases of a more general model. The course concentrates on research designs and analysis for problems in which there is a single outcome variable and would be taught using the general linear model as a framework to design and analysis.<br><br>Content covered includes: Introduction to the use of statistical methods in research; Introduction to R for statistics; Refresher in inferential statistics including Hypothesis testing, Type I vs. Type II errors, p-values, power, correlation, chi-squares; Linear regression: including diagnostics, transformation, different families of models; Multiple regression: extending linear regression to multiple IVs and including interactions, effects coding; The general linear model (GLM) as an inclusive framework (including ANOVA, ANCOVA, mixed designs)',
+       'dapr2':'Data Analysis in Psychology using R-2 will arrive in the 2020-21 academic year.',
+       'dapr3':'Data Analysis in Psychology using R-3 will arrive in the 2021-22 academic year.',
+       
+       'usmr':'Univariate Statistics & Methodology in R is a semester long crash-course which starts with an introduction to basic statistics and the fundamentals of R programming, and aims to provide students with a competence in standard univariate methodology and analysis using R.<br><br>Design and analysis are taught under a unifying framework which shows a) how research problems and design should inform which statistical method to use and b) that all statistical methods are special cases of a more general model. The course concentrates on research designs and analysis for problems in which there is a single outcome variable and would be taught using the general linear model as a framework to design and analysis.<br><br>Content covered includes: Introduction to the use of statistical methods in research; Introduction to R for statistics; Refresher in inferential statistics including Hypothesis testing, Type I vs. Type II errors, p-values, power, correlation, chi-squares; Linear regression: including diagnostics, transformation, different families of models; Multiple regression: extending linear regression to multiple IVs and including interactions, effects coding; The general linear model (GLM) as an inclusive framework (including ANOVA, ANCOVA, mixed designs)',
        
        'multivar':'Multivariate Statistics & Methodology in R is a semester long course providing an advanced level overview of statistical analysis techniques and methodology issues relevant to psychological research. The course builds on the concepts and skills developed in Univariate Statistics and Methodology using R.<br><br>Multivariate Statistics and Methodology using R uses a problem-oriented approach to introduce analysis tools that extend to cases where multiple outcome variables are being studied simultaneously, and cases where the data contain a structure, e.g. children nested in classes, in schools, or more complex cases.<br><br>Content covered includes: Linear Mixed Modelling; Growth Curve Analysis; Principal Component Analysis; Factor Analysis; Path Analysis; Structural Equation Modelling',
        
@@ -125,7 +127,14 @@ ddesc={'': 'The Department of Psychology at the University of Edinburgh offers 3
 def fun(dir,rootdir):
     print('Processing: '+dir)
     filenames = [fname for fname in sorted(os.listdir(dir)) if fname not in EXCLUDED and os.path.isfile(dir+fname)]
-    dirnames = [fname for fname in sorted(os.listdir(dir)) if fname not in EXCLUDED]
+    
+    # this is for recursively indexing
+    subdirnames = [fname for fname in sorted(os.listdir(dir)) if fname not in EXCLUDED]
+    subdirnames = [fname for fname in subdirnames if fname not in filenames]
+    subdirnames = [fname for fname in subdirnames if fname not in EXCLUDEDDIR]
+    
+    # this is for the lists
+    dirnames = [fname for fname in sorted(os.listdir(dir)) if fname in INCLUDED]
     dirnames = [fname for fname in dirnames if fname not in filenames]
     dirorder = ["dapr1","dapr2","dapr3","usmr","multivar"]
     dirnames = [x for x in dirorder for y in dirnames if y == x] + [y for y in dirnames if y not in dirorder]
@@ -147,7 +156,7 @@ def fun(dir,rootdir):
                                           ROOTDIR=rootdir,time=time.ctime(os.path.getctime(dir))),file=f)
     f.close()
     
-    for subdir in [dirnames for dirnames in dirnames if dirnames not in EXCLUDEDDIR]:
+    for subdir in subdirnames:
         try:
             fun(dir+subdir+"/",rootdir+'../')
         except:
